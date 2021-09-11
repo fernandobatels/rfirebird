b84f697bbbf6a8c0e7e5cba739017f19db855d42
Luis Fernando Batels <luisfbatels@gmail.com>
2021-09-11 08:28:45 -0300

    Uso de buffer
    A struct Database foi alterada para utilizar o buffer do arquivo do
    db em vez de efetuar a leitura completa deste e armazenar todo o seu
    conteúdo.

--- firebirdreader/lfirebirdreader/src/database.rs
+++ firebirdreader/lfirebirdreader/src/database.rs
@@ -1,6 +1,8 @@
 //! Firebird database representation
 
 use std::io::{Read, BufReader};
+use std::rc::Rc;
+use std::cell::RefCell;
 use std::fs::File;
 
 use rsfbclient_core::FbError;
@@ -11,27 +13,24 @@ use crate::page::HeaderPage;
 /// The Firebird database
 pub struct Database {
     pub header: HeaderPage,
-    raw: Vec<u8>
+    buffer: Rc<RefCell<BufReader<File>>>
 }
 
 impl Database {
 
     /// Read the database from a buffer
-    pub fn open(bf_reader: &mut BufReader<File>) -> Result<Database, FbError> {
+    pub fn open(buffer: Rc<RefCell<BufReader<File>>>) -> Result<Database, FbError> {
 
         let header = {
             let mut tag = [0 as u8; 1024];
-            bf_reader.read_exact(&mut tag)?;
+            buffer.borrow_mut().read_exact(&mut tag)?;
 
             HeaderPage::from_bytes(tag)?
         };
 
-        let mut raw = vec![];
-        bf_reader.read_to_end(&mut raw)?;
-
         Ok(Self {
             header,
-            raw
+            buffer
         })
     }
 
@@ -39,9 +38,9 @@ impl Database {
     pub fn open_file(fpath: &str) -> Result<Database, FbError> {
         let f = File::open(fpath)
             .map_err(|e| e.to_string())?;
-        let mut bfr = BufReader::new(f);
+        let bfr = BufReader::new(f);
 
-        Database::open(&mut bfr)
+        Database::open(Rc::new(RefCell::new(bfr)))
     }
 
     pub fn tables(&mut self) -> Result<Vec<Table>, FbError> {
